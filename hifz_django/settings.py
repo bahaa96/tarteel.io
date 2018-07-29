50ea8e177469609a979bd03228edd27cae9514ca
Abdul Alfozan <alfozan111@gmail.com>
2018-07-29 14:16:35 -0700

    remove django_heroku

--- hifz_django/settings.py
+++ hifz_django/settings.py
@@ -10,9 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import django_heroku
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,5 +125,3 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-django_heroku.settings(locals())
