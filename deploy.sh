e4893f8c06313113bba14f7cf1f2fef2817ba104
Abdul Alfozan <alfozan111@gmail.com>
2018-07-29 14:33:48 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,29 +3,10 @@
 # exit on any failure
 set -e
 
-# usage:
-usage() {
-  cat 1>&2 <<EOF
-./deploy production
-./deploy development DB_NAME
-EOF
-  exit 0;
-}
-[ -z "$1" ] && usage
 
-DJANGO_ENV=$1   # production/development
-if [ "$DJANGO_ENV" == "production" ]; then
-  REPO=tarjimly-matching-learnables
-  PORT=9015
-  args+=(--log-driver=journald --restart=always --cpuset-cpus=4,5)
-elif [ "$DJANGO_ENV" == "development" ]; then
-  [[ -z "$2" ]] && usage;
-  REPO=dev-tarjimly-matching-learnables
-  PORT=9016
-  args+=(--env DB_NAME=$2 --cpuset-cpus=6)
-else
-  usage
-fi
+REPO=tarteel
+PORT=4444
+args+=(--restart=always --cpuset-cpus=6)
 
 # stop and remove existing container
 if [ -n "$(docker ps --all --quiet --filter name=^/${REPO}$)" ]; then
@@ -45,11 +26,10 @@ args+=(
   --workdir=/app
   --detach
   --publish 127.0.0.1:$PORT:8000
-  --env DJANGO_ENV=${DJANGO_ENV}
   --env TERM=xterm-256color
   --tty
   python:3
-  /bin/sh -c "pip3 install -r requirements.txt && (./manage.py runserver 0.0.0.0:8000 & ./cacher.py)"
+  /bin/sh -c "pip3 install -r requirements.txt && ./manage.py makemigrations && ./manage.py migrate && ./manage.py runserver 0.0.0.0:8000"
 )
 docker run "${args[@]}"
 
